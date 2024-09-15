9e977f1e68e1a9a1bd459321e069ce6c5a050f9f
INswon <152695318+INswon@users.noreply.github.com>
2024-09-15 23:46:37 +0900

    Update urls.py
    アプリケーションパス修正

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import SignupView 
+from .views import SignupView, CustomLogoutView
 from . import views
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('home/',views.home, name="home"),
-    path('profile/',views.profile, name="profile"),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
