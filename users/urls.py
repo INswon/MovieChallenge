999ba7dfd7a86087a67453fdc3d4ebd5f4334b41
INswon <152695318+INswon@users.noreply.github.com>
2024-09-18 16:24:30 +0900

    Update urls.py
    映画鑑賞記録の新規作成、一覧表示、削除ビューのURLパスを追加

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SignupView, CustomLogoutView
+from .views import SignupView, CustomLogoutView, MovieRecordListView, MovieRecordCreateView, MovieRecordDeleteView
 from . import views
 from django.contrib.auth import views as auth_views
 from django.conf import settings
@@ -8,6 +8,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('home/',views.home, name="home"),
+    path('list/',MovieRecordListView.as_view(), name='list'),
+    path('create/',MovieRecordCreateView.as_view(), name="create"),
+    path('delete/<int:pk>/',MovieRecordDeleteView.as_view(), name="delete"),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
