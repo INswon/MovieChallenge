16f36be8786a41167a56a454668824ab64869ced
INswon <152695318+INswon@users.noreply.github.com>
2024-09-20 23:32:05 +0900

    Move existing URL paths from users_app to movies_app
    修正機能のパスを新規に追加

--- movies/urls.py
+++ movies/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+from .views import MovieRecordListView, MovieRecordCreateView, MovieRecordDeleteView,MovieRecordEditView
 
 urlpatterns = [
-    path('',views.TimelineView.as_view(), name='timeline_view'),
+    path('list/',MovieRecordListView.as_view(), name='list'),
+    path('create/',MovieRecordCreateView.as_view(), name="create"),
+    path('delete/<int:pk>/',MovieRecordDeleteView.as_view(), name="delete"),
+    path('edit/<int:pk>/', MovieRecordEditView.as_view(), name='movie_edit'),
 ]
