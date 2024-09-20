6e465c75d7f72c9e50a7dc72a7bc3603d7e1f97c
INswon <152695318+INswon@users.noreply.github.com>
2024-09-21 00:07:30 +0900

    Update views.py
    不要なビューを削除
    編集機能のビューを追加

--- movies/views.py
+++ movies/views.py
@@ -1,15 +1,40 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
-from .models import UserMovieRecord
+from django.views.generic.edit import UpdateView
+from movies.models import UserMovieRecord
+from django.views.generic import ListView, CreateView 
+from django.views.generic.edit import DeleteView 
+from .forms import MovieRecordForm
+from django.urls import reverse_lazy
 
-class TimelineView(LoginRequiredMixin, ListView):
+class MovieRecordListView(ListView):
     model = UserMovieRecord
-    template_name = 'timeline.html'
-    context_object_name = 'watch_history'
-    ordering = ['-watched_on']
-    
+    template_name = 'users/movie_record_list.html'
+    context_object_name = 'records'
+
     def get_queryset(self):
         return UserMovieRecord.objects.filter(user=self.request.user)
-    
 
+class MovieRecordCreateView(CreateView):
+    model = UserMovieRecord
+    form_class = MovieRecordForm  
+    template_name = 'users/movie_record_create.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        print(form.errors)
+        return super().form_invalid(form)
     
+class MovieRecordDeleteView(DeleteView):
+    model = UserMovieRecord
+    template_name = 'users/movie_record_delete.html'
+    success_url = reverse_lazy('home') 
+
+class MovieRecordEditView(UpdateView):
+    model = UserMovieRecord
+    form_class = MovieRecordForm
+    template_name = 'movies/movie_record_edit.html'
+    success_url = reverse_lazy('home')
+
