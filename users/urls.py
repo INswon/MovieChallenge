88eccc7ec225b39c0109c15ba77cc633a1de0da4
INswon <152695318+INswon@users.noreply.github.com>
2024-09-24 23:30:05 +0900

    Update urls.py
    Home画面をクラスベースビュー(ListView)に修正

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from .views import SignupView, CustomLogoutView
+from .views import SignupView, CustomLogoutView, UserMovieListview
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('home/',views.home, name="home"),
+    path('home/', UserMovieListview.as_view(),name='home'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
 ]
