e723bdd9606ddd2f4ef62f21b8424f7d5f84b966
INswon <152695318+INswon@users.noreply.github.com>
2024-09-30 00:14:43 +0900

    Update urls.py
    usersアプリケーションのURLパターンに名前空間を追加

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from . import views
 from .views import SignupView, CustomLogoutView, UserMovieListview
 from django.contrib.auth import views as auth_views
 
+app_name = 'users'
+
 urlpatterns = [
-    path('', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('home/', UserMovieListview.as_view(),name='home'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
