af85c5a030248111edb58edb6938e9e24c2bc0ca
INswon <152695318+INswon@users.noreply.github.com>
2024-09-30 21:29:02 +0900

    Update views.py
    未承認の映画記録削除に対してLoginRequiredMixinと403 Forbiddenレスポンスを追加
    
    - ユーザー認証のためにMovieRecordDeleteViewにLoginRequiredMixinを追加
    - 他のユーザーの映画記録を削除しようとした場合に403 Forbiddenを返すロジックを実装

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
 from movies.models import UserMovieRecord
 from django.views.generic import ListView, CreateView 
 from django.views.generic.edit import DeleteView 
 from .forms import MovieRecordForm
+from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
 
 class MovieRecordListView(ListView):
@@ -17,7 +19,7 @@ class MovieRecordCreateView(CreateView):
     model = UserMovieRecord
     form_class = MovieRecordForm  
     template_name = 'movies/movie_record_create.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('users:home')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -27,14 +29,20 @@ class MovieRecordCreateView(CreateView):
         print(form.errors)
         return super().form_invalid(form)
     
-class MovieRecordDeleteView(DeleteView):
+class MovieRecordDeleteView(LoginRequiredMixin, DeleteView):
     model = UserMovieRecord
     template_name = 'movies/movie_record_delete.html'
-    success_url = reverse_lazy('home') 
+    success_url = reverse_lazy('users:home')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.user != request.user:
+            return HttpResponseForbidden()  # 403 Forbidden
+        return super().delete(request, *args, **kwargs)
 
 class MovieRecordEditView(UpdateView):
     model = UserMovieRecord
     form_class = MovieRecordForm
     template_name = 'movies/movie_record_edit.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('users:home')
 
