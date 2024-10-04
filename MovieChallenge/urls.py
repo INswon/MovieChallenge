ca0d14a17b8b39246db1d14864ba7d64cc4c28f2
INswon <152695318+INswon@users.noreply.github.com>
2024-10-04 23:48:23 +0900

    Update urls.py
    カスタム404ビューの設定
    
    - カスタム404エラーハンドラーを'users.views.custom_404_view' に設定

--- MovieChallenge/urls.py
+++ MovieChallenge/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
+handler404 = 'users.views.custom_404_view'  # カスタム404ビューの指定
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include(('users.urls', 'users'), namespace='users')), 
