21a075f2906cafe916dd9b05e983cda0ffc0a8ef
INswon <152695318+INswon@users.noreply.github.com>
2024-10-08 23:19:06 +0900

    Update models.py
    映画鑑賞目標設定機能モデル実装(ProgressGoal)
    - STATUS_CHOICESフィールドで目標の状態を、active(進行中),
    completed(達成済み), paused(保留)で設定
    -タイトル、目標達成日、現在の進捗、目標映画数、ジャンル、鑑賞済み映画リスト、評価や感想フィールドを含む
    - 目標とユーザーを関連付けるため、ForeignKeyでUserモデルを参照

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class ProgressGoal(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'Active'),
+        ('completed', 'Completed'),
+        ('paused', 'paused'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal_title = models.CharField(max_length=200)  # 目標タイトル
+    target_date = models.DateField()  # 目標達成日
+    current_progress = models.IntegerField(default=0)  # 現在の進捗状況
+    total_movies = models.IntegerField()  # 目標の映画鑑賞数
+    genre_preferences = models.CharField(max_length=200, blank=True)  # ジャンルの好み
+    status = models.CharField(max_length=20, choices=  STATUS_CHOICES, default='active')  # 目標の状態
+    movies_watched = models.TextField(blank=True)  # 鑑賞した映画のリスト
+    movie_ratings = models.TextField(blank=True)  # 映画の評価や感想
+
+   
+
+    def __str__(self):
+        return f"{self.goal_title} - {self.user.username}"
