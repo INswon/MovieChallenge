30125d521bbd702efb19fe7a76455090ae04a616
INswon <152695318+INswon@users.noreply.github.com>
2024-10-08 23:43:09 +0900

    Update urls.py
    映画鑑賞目標新規設定、更新設定のURL設定
    
    - ProgressGoalCreateViewへのパスを追加 
        目標の新規作成ページを提供
    
    - ProgressGoalUpdateViewへのパスを追加 
       目標の進捗更新ページを提供

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from .views import SignupView, CustomLogoutView, UserMovieListview
+from .views import SignupView, CustomLogoutView, UserMovieListview, ProgressGoalCreateView, ProgressGoalUpdateView
 from django.contrib.auth import views as auth_views
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('home/', UserMovieListview.as_view(),name='home'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('home/', UserMovieListview.as_view(),name='home'),
+    path('goals/create/',ProgressGoalCreateView.as_view(), name='progress_goal_create'),
+    path('goals/update/<int:pk>/', ProgressGoalUpdateView.as_view(), name='progress_goal_update'),
 ]
