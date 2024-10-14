2755e5fda1053f6bc9fe72be8a8f610c44cb558d
INswon <152695318+INswon@users.noreply.github.com>
2024-10-14 10:17:23 +0900

    Update urls.py
    URLパターンに名前空間を追加
    
    -missionsアプリケーションにそれぞれ名前空間 
    (namespace) を追加

--- MovieChallenge/urls.py
+++ MovieChallenge/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from movies.views import YourView  
 
 handler404 = 'users.views.custom_404_view'  # カスタム404ビューの指定
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include(('users.urls', 'users'), namespace='users')), 
-    path('movie/', include(('movies.urls'), namespace='movies')),  
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+    path('movie/', include(('movies.urls', 'movies'), namespace='movies')),  
+    path('missons/', include(('missions.urls', 'missions'), namespace='missions')),  
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
