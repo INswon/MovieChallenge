7620521d9be5e817ea458830212755fb28e56147
INswon <152695318+INswon@users.noreply.github.com>
2025-02-05 21:16:33 +0900

    feat: 映画鑑賞記録機能の拡張 & ビューの整理
    - `UserMovieListView` を追加し、映画鑑賞一覧ページ (`home`) を設定
    - `MovieRecordDetailView` を `views.py` に追加し、詳細画面を実装
    - `MovieRecordCreateView` の `form_valid` にバッチ付与処理の仮実装を追加（今後調整予定）
    - `MovieRecordDeleteView` に論理削除機能を実装し、バッジ削除条件を適用（バッジ削除の閾値調整は今後検討）
    - `movies:home` へ適切にリダイレクトするよう `success_url` を統一 (`MovieRecordEditView` を除く)

--- movies/views.py
+++ movies/views.py
@@ -1,48 +1,72 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView
-from movies.models import UserMovieRecord
-from django.views.generic import ListView, CreateView 
-from django.views.generic.edit import DeleteView 
-from .forms import MovieRecordForm
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from django.http import HttpResponseForbidden
 from django.urls import reverse_lazy
+from movies.models import UserMovieRecord
+from missions.models import Batch, UserBatch
+from .forms import MovieRecordForm
+from django.views import View
+from django.shortcuts import render
+from missions.models import Mission
+from missions.utils import MissionCompletionHandler
 
-class MovieRecordListView(ListView):
+#映画鑑賞記録一覧表示機能
+class UserMovieListView(LoginRequiredMixin, ListView):
     model = UserMovieRecord
-    template_name = 'movies/movie_record_list.html'
+    template_name = 'movies/home.html'
     context_object_name = 'records'
 
-    def get_queryset(self):
-        return UserMovieRecord.objects.filter(user=self.request.user)
+#映画鑑賞記録詳細表示機能
+class MovieRecordDetailView(DetailView):
+    model = UserMovieRecord
+    template_name = 'movies/movie_record_detail.html'  
+    context_object_name = 'record'
 
+#映画鑑賞記録新規作成機能
 class MovieRecordCreateView(CreateView):
     model = UserMovieRecord
-    form_class = MovieRecordForm  
+    form_class = MovieRecordForm
     template_name = 'movies/movie_record_create.html'
-    success_url = reverse_lazy('users:home')
+    success_url = reverse_lazy('movies:home')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super().form_valid(form)
+        response = super().form_valid(form)
 
-    def form_invalid(self, form):
-        print(form.errors)
-        return super().form_invalid(form)
-    
+        MissionCompletionHandler.assign_batch(self.request.user)
+        return response
+
+# 映画鑑賞記録削除機能
 class MovieRecordDeleteView(LoginRequiredMixin, DeleteView):
     model = UserMovieRecord
     template_name = 'movies/movie_record_delete.html'
-    success_url = reverse_lazy('users:home')
+    success_url = reverse_lazy('movies:home') 
 
     def delete(self, request, *args, **kwargs):
         self.object = self.get_object()
         if self.object.user != request.user:
-            return HttpResponseForbidden()  # 403 Forbidden
+            return HttpResponseForbidden()  
+        
+        self.object.is_deleted = True
+        self.object.save()
+        
+        self.remove_batch_if_ineligible(request.user)
+        
         return super().delete(request, *args, **kwargs)
 
-class MovieRecordEditView(UpdateView):
+    @classmethod
+    def remove_batch_if_ineligible(cls, user):
+        #鑑賞数が条件を満たさなくなった場合、バッジを取り消す
+
+        watched_count = UserMovieRecord.objects.filter(user=user, is_deleted=False).count()
+        if watched_count < 3:  
+            UserBatch.objects.filter(user=user, batch__name="鑑賞3作品達成バッジ").delete()
+            print("バッジが削除されました：鑑賞3作品達成バッジ")
+
+
+#映画鑑賞編集機能
+class MovieRecordEditView(LoginRequiredMixin, UpdateView):
     model = UserMovieRecord
     form_class = MovieRecordForm
     template_name = 'movies/movie_record_edit.html'
     success_url = reverse_lazy('users:home')
-
