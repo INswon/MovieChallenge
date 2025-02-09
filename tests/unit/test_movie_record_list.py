1aef8a4dafab75d6dc7ab86f3272f26ba954ceec
INswon <152695318+INswon@users.noreply.github.com>
2025-02-09 22:07:18 +0900

    fix: ユーザー認証時のリダイレクト処理を調整
    - reverse('home') の呼び出しを reverse('movies:home') に変更
    - reverse('users:login') が /users/ を返す仕様だったため、適切に対応
    - settings.LOGIN_URL を使用し、環境依存しないリダイレクト先に統一
    - assertContains のメッセージをDjangoの日本語エラーメッセージと一致させるよう調整

--- tests/unit/test_movie_record_list.py
+++ tests/unit/test_movie_record_list.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from movies.models import UserMovieRecord
 from django.utils import timezone
+from django.conf import settings
 import datetime
 
 #一覧画面のテストケース作成
@@ -31,20 +32,19 @@ class UserMovieListViewTest(TestCase):
     def test_logged_in_user_can_access(self):
         # ユーザー登録しているユーザーがhomeページにアクセスできるか確認
         self.client.login(username = 'testuser', password = 'testpass')
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('movies:home'))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'users/home.html')
+        self.assertTemplateUsed(response, 'movies/home.html')
 
 
     def test_redirect_for_unauthenticated_user(self):
-        # homeページにログイン情報を入力せずにアクセスした場合
-        response = self.client.get(reverse('home'))
-        self.assertRedirects(response, f"{reverse('login')}?next=/users/home/")
-
+        response = self.client.get(reverse('movies:home'))
+        expected_url = f"{settings.LOGIN_URL}?next={reverse('movies:home')}"  
+        self.assertRedirects(response, expected_url, fetch_redirect_response=False)
 
     def test_login_with_invalid_credentials(self):
         # homeページに誤ったユーザー名とパスワードでアクセスした場合
-        response = self.client.post(reverse('login'), {
+        response = self.client.post(reverse('users:login'), {
             'username': 'wronguser',
             'password': 'wrongpass'
         })
@@ -52,13 +52,13 @@ class UserMovieListViewTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'users/login.html')
 
-        self.assertContains(response, "Please enter a correct username and password. Note that both fields may be case-sensitive.")
+        self.assertContains(response,  "正しいユーザー名とパスワードを入力してください。どちらのフィールドも大文字と小文字は区別されます。")
 
 
     def test_context_data(self):
         # コンテキストに含まれているデータが正しいか確認
         self.client.login(username = 'testuser', password = 'testpass')
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse('movies:home'))
         
         # コンテキスト内の'records'に正しいデータが含まれていることを確認
         self.assertIn('records', response.context)
