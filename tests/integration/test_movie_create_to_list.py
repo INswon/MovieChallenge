1874b95bbd823ba9b73dcbdfa331a81c543653cd
INswon <152695318+INswon@users.noreply.github.com>
2025-02-09 21:52:24 +0900

    revert: 誤削除したテストフォルダーを復元

---
+++ tests/integration/test_movie_create_to_list.py
@@ -0,0 +1,62 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from movies.models import UserMovieRecord
+from django.utils import timezone
+
+class MovieCreateToListIntegrationTest(TestCase):
+
+    #事前準備: ユーザー作成 & ログイン
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+
+    #映画記録を作成後、リダイレクトされるか確認
+    def test_create_movie_and_redirect(self):
+        
+        self.client.login(username='testuser', password='testpass') 
+
+        response = self.client.post(reverse('movies:create'), {
+            'title': 'Test Movie',
+            'rating': 5,
+            'date_watched': timezone.now().date(),
+        })
+
+        self.assertEqual(response.status_code, 302)  
+
+    #映画記録を作成後、一覧ページに表示されるか確認
+    def test_create_movie_and_check_list(self):
+        
+        self.client.login(username='testuser', password='testpass')  
+
+        # 映画を作成
+        self.client.post(reverse('movies:create'), {
+            'title': 'Test Movie',
+            'rating': 5,
+            'date_watched': timezone.now().date(),
+        })
+
+        print("📌 DB内の映画データ:", UserMovieRecord.objects.all())  
+
+        response = self.client.get(reverse('movies:home'))
+
+        print("📌 レスポンスのHTML:", response.content.decode())
+
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, 'Test Movie')
+
+
+    #未ログインユーザーは映画を作成できず、ログインページにリダイレクトされるか確認
+    def test_unauthenticated_user_cannot_create_movie(self):
+    
+        self.client.logout()  
+
+        response = self.client.post(reverse('movies:create'), {
+            'title': 'Unauthorized Movie',
+            'rating': 3,
+            'date_watched': timezone.now().date(),
+        })
+
+        self.assertEqual(response.status_code, 302)
+        self.assertIn(reverse('users:login'), response.url)
+
---
+++ tests/unit/test_movie_record_list.py
@@ -0,0 +1,68 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from movies.models import UserMovieRecord
+from django.utils import timezone
+import datetime
+
+#一覧画面のテストケース作成
+class UserMovieListViewTest(TestCase):
+
+    def setUp(self):
+        #　ユーザーとデータのセットアップ
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.movie1 = UserMovieRecord.objects.create(
+            user = self.user,
+            title = 'Movie 1', 
+            poster = 'media/posters/test_movie_poster.jpg', 
+            date_watched = timezone.now(),
+            rating = 5
+        )
+
+        self.movie2 = UserMovieRecord.objects.create(
+            user = self.user,
+            title = 'Movie 2',
+            poster = 'media/posters/test_movie_poster2.jpg',  
+            date_watched = datetime.datetime(2024, 1, 2, tzinfo=datetime.timezone.utc),
+            rating = 4
+        )
+
+
+    def test_logged_in_user_can_access(self):
+        # ユーザー登録しているユーザーがhomeページにアクセスできるか確認
+        self.client.login(username = 'testuser', password = 'testpass')
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/home.html')
+
+
+    def test_redirect_for_unauthenticated_user(self):
+        # homeページにログイン情報を入力せずにアクセスした場合
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, f"{reverse('login')}?next=/users/home/")
+
+
+    def test_login_with_invalid_credentials(self):
+        # homeページに誤ったユーザー名とパスワードでアクセスした場合
+        response = self.client.post(reverse('login'), {
+            'username': 'wronguser',
+            'password': 'wrongpass'
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/login.html')
+
+        self.assertContains(response, "Please enter a correct username and password. Note that both fields may be case-sensitive.")
+
+
+    def test_context_data(self):
+        # コンテキストに含まれているデータが正しいか確認
+        self.client.login(username = 'testuser', password = 'testpass')
+        response = self.client.get(reverse('home'))
+        
+        # コンテキスト内の'records'に正しいデータが含まれていることを確認
+        self.assertIn('records', response.context)
+        records = response.context['records']
+        self.assertEqual(len(records), 2) 
+        self.assertEqual(records[0], self.movie1)
+        self.assertEqual(records[1], self.movie2)
