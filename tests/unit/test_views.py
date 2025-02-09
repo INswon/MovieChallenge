3b63f16ab0a3ab6abaad5973c1f4b45d77c0199c
INswon <152695318+INswon@users.noreply.github.com>
2025-02-09 21:34:45 +0900

    chore: 不要なテストファイルを削除
    -'tests/unit/` に移動の為

--- tests/unit/test_views.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from django.urls import reverse
-
-class YourViewTests(TestCase):
-    def test_existing_url_view(self):
-        response = self.client.get(reverse('existing_url'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'data here')  # 渡したデータが表示されることを確認
