b870710d359b9f4e56a88014f39ad3f9bbad2056
INswon <152695318+INswon@users.noreply.github.com>
2025-02-10 22:56:26 +0900

    feat: 映画削除機能の統合テストを追加
    - 削除ボタン押下時に確認画面が表示されることを検証
    - 削除をキャンセルすると映画が削除されないことを検証
    - 存在しない映画を削除しようとすると404エラーになることを検証
    - 他のユーザーが別のユーザーの映画を削除できないことを検証

---
+++ tests/integration/test_movie_delete_to_list.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core.files.base import ContentFile
+from django.contrib.auth.models import User
+from movies.models import UserMovieRecord
+from django.utils import timezone
+from PIL import Image
+import io
+
+#JPEG画像テストデータ作成
+def create_dummy_image():
+    image = Image.new("RGB", (1,1), color="white")
+    img_io = io.BytesIO()
+    image.save(img_io, format="JPEG")
+    img_io.seek(0)
+    return ContentFile(img_io.getvalue(), name="test_poster.jpg")
+
+poster_file = create_dummy_image()
+
+
+#削除機能と結合テスト
+class MovieDeleteIntegrationTest(TestCase):
+     
+     #テストデータ作成
+     def setUp(self):
+          self.user = User.objects.create_user(username='testuser', password='testpass')
+          self.another_user = User.objects.create_user(username='anotheruser', password='anotherpass')
+
+          self.movie = UserMovieRecord.objects.create(
+               user=self.user,
+               title="Test Movie",
+               date_watched=timezone.now().date(),
+               rating=5
+            )
+          
+          self.client.login(username='testuser', password='testpass')
+
+
+     #削除ボタン押下で削除確認画面が表示されるか検証
+     def test_delete_movie_and_redirect(self):
+        response = self.client.post(reverse('movies:delete', args=[self.movie.id]))
+        self.assertRedirects(response, reverse('movies:home')) 
+
+
+     # 削除確認画面でキャンセルボタンを押すと、削除されず元のページに戻ることの検証
+     def test_cancel_delete_does_not_remove_movie(self):
+         response = self.client.get(reverse('movies:delete', args=[self.movie.id]))
+         self.assertEqual(response.status_code, 200)
+
+     # 削除対象の映画鑑賞記録が存在しない場合、404エラーになることの検証
+     def test_delete_nonexistent_movie_returns_404(self):
+        response = self.client.post(reverse('movies:delete', args=[99999]))
+        self.assertEqual(response.status_code, 404)
+
+     #他のユーザーが映画鑑賞記録を削除できないことの検証
+     def test_other_user_cannot_delete_movie(self):
+        self.client.logout()
+        self.client.login(username='anotheruser', password='anotherpass')
+
+        response = self.client.post(reverse('movies:delete', args=[self.movie.id]))
+
+        if response.status_code == 302:
+            self.assertTrue(response.url.startswith(reverse('movies:home')))
+        else:
+            self.assertEqual(response.status_code, 403)
