9f34ffef2469179d6fffe3a25d16df55d8007ada
INswon <152695318+INswon@users.noreply.github.com>
2025-02-13 16:44:42 +0900

    Feat: ポスター削除用のdelete_posterフィールドを追加し、削除処理を改善
    - ユーザーがポスター削除を明示できるdelete_poster BooleanFieldを追加
    - save()メソッド内で、delete_poster=Trueの場合にposter.delete()を実行し、poster=Noneをセット
    - 削除したポスターがデータベースに残らないように修正

--- movies/forms.py
+++ movies/forms.py
@@ -2,6 +2,9 @@ from django import forms
 from .models import UserMovieRecord
 
 class MovieRecordForm(forms.ModelForm):
+
+    delete_poster = forms.BooleanField(required=False, label="ポスター画像を削除") 
+    
     class Meta:
         model = UserMovieRecord
         fields = ['title', 'poster', 'rating', 'date_watched']
@@ -28,6 +31,20 @@ class MovieRecordForm(forms.ModelForm):
         if rating not in range(1, 6):
             raise forms.ValidationError("評価は1から5の間で選択してください。")
         return rating
+    
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        
+        if self.cleaned_data.get("delete_poster"):
+            if instance.poster:
+                instance.poster.delete(save=False)
+            instance.poster = None  
+
+        if commit:
+            instance.save()
+        return instance
+    
+
 
     def __init__(self, *args, **kwargs):
         super(MovieRecordForm, self).__init__(*args, **kwargs)
