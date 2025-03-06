d481d945d855587a576218c7f191f89b7ba8cc47
INswon <152695318+INswon@users.noreply.github.com>
2025-03-06 17:26:27 +0900

    refactor: バッジ処理のロジックを削除し、シンプルな実装に変更
    - 映画鑑賞記録の作成時にバッジ処理（MissionCompletionHandler.assign_batch）を削除
    - 記録削除時のバッジ削除ロジックを削除
    - import 文から不要になった  MissionCompletionHandler を削除

--- movies/views.py
+++ movies/views.py
@@ -8,24 +8,23 @@ from .forms import MovieRecordForm
 from django.views import View
 from django.shortcuts import render
 from missions.models import Mission
-from missions.utils import MissionCompletionHandler
 
-#映画鑑賞記録一覧表示機能
+# 映画鑑賞記録一覧表示機能
 class UserMovieListView(LoginRequiredMixin, ListView):
     model = UserMovieRecord
     template_name = 'movies/home.html'
     context_object_name = 'records'
-    
+
     def get_queryset(self):
         return UserMovieRecord.objects.filter(user=self.request.user)
 
-#映画鑑賞記録詳細表示機能
-class MovieRecordDetailView(LoginRequiredMixin,DetailView):
+# 映画鑑賞記録詳細表示機能
+class MovieRecordDetailView(LoginRequiredMixin, DetailView):
     model = UserMovieRecord
     template_name = 'movies/movie_record_detail.html'  
     context_object_name = 'record'
 
-#映画鑑賞記録新規作成機能
+# 映画鑑賞記録新規作成機能
 class MovieRecordCreateView(LoginRequiredMixin, CreateView):
     model = UserMovieRecord
     form_class = MovieRecordForm
@@ -34,21 +33,11 @@ class MovieRecordCreateView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        
+
         if not form.cleaned_data.get("poster"):
             form.instance.poster = None
-        
-        response = super().form_valid(form)
 
-        try:
-            print("バッジ処理開始")
-            MissionCompletionHandler.assign_batch(self.request.user)
-            print("バッジ処理完了")
-        except Exception as e:
-            print("バッジ処理エラー:", e)
-
-        return response
-
+        return super().form_valid(form)
 
 # 映画鑑賞記録削除機能
 class MovieRecordDeleteView(LoginRequiredMixin, DeleteView):
@@ -64,21 +53,9 @@ class MovieRecordDeleteView(LoginRequiredMixin, DeleteView):
         self.object.is_deleted = True
         self.object.save()
         
-        self.remove_batch_if_ineligible(request.user)
-        
         return super().delete(request, *args, **kwargs)
 
-    @classmethod
-    def remove_batch_if_ineligible(cls, user):
-        #鑑賞数が条件を満たさなくなった場合、バッジを取り消す
-
-        watched_count = UserMovieRecord.objects.filter(user=user, is_deleted=False).count()
-        if watched_count < 3:  
-            UserBatch.objects.filter(user=user, batch__name="鑑賞3作品達成バッジ").delete()
-            print("バッジが削除されました：鑑賞3作品達成バッジ")
-
-
-#映画鑑賞編集機能
+# 映画鑑賞編集機能
 class MovieRecordEditView(LoginRequiredMixin, UpdateView):
     model = UserMovieRecord
     form_class = MovieRecordForm
@@ -86,13 +63,13 @@ class MovieRecordEditView(LoginRequiredMixin, UpdateView):
     success_url = reverse_lazy('movies:home')
 
     def save(self, commit=True):
-          instance = super().save(commit=False)
-          
-          if self.cleaned_data.get("delete_poster"):
+        instance = super().save(commit=False)
+        
+        if self.cleaned_data.get("delete_poster"):
             if instance.poster:
                 instance.poster.delete(save=False)
             instance.poster = None
 
-          if commit:
-              instance.save()
-          return instance
+        if commit:
+            instance.save()
+        return instance
