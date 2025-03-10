9f78e59013e138c6787223cfac858bd087695f07
INswon <152695318+INswon@users.noreply.github.com>
2025-03-11 00:33:20 +0900

    fix: ミッション達成時に is_completed が適切に更新されない問題を修正
    - UserMission の is_completed が適切に更新されるよう修正
    - grant_batch_for_mission() で UserMission の状態を再確認し、確実に更新するよう修正
    - UserBatch の付与処理が適切に実行されるよう修正

--- missions/services.py
+++ missions/services.py
@@ -2,7 +2,6 @@ from movies.models import UserMovieRecord
 from missions.models import Mission, UserMission, UserBatch, Batch
 from django.utils.timezone import now
 
-#ミッション達成のチェックとバッジ付与処理
 class MissionService:
     @staticmethod
     def check_and_complete_mission(user, mission_title):
@@ -11,7 +10,8 @@ class MissionService:
 
         if watched_movies_count >= 3:
             user_mission, created = UserMission.objects.get_or_create(user=user, mission=mission)
-            if not user_mission.is_completed:
+
+            if created or not user_mission.is_completed:
                 user_mission.is_completed = True
                 user_mission.completed_at = now()
                 user_mission.save()
@@ -21,12 +21,14 @@ class MissionService:
 
         return False, "まだミッション達成に必要な視聴数に到達していません"
 
-    #ミッション達成時に、バッジを付与する処理
     @staticmethod
     def grant_batch_for_mission(user, mission_title):
         mission = Mission.objects.get(title=mission_title)
+        user_mission, created = UserMission.objects.get_or_create(user=user, mission=mission)
+        if not user_mission.is_completed:
+            user_mission.is_completed = True
+            user_mission.completed_at = now()
+            user_mission.save()
 
         if mission.batch:
-            user_batch, created = UserBatch.objects.get_or_create(user=user, batch=mission.batch)
-        else:
-            return
+            UserBatch.objects.get_or_create(user=user, batch=mission.batch)
