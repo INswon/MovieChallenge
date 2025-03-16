23a5f64da46c0ebf5b47864563553a5d4f7fd66c
INswon <152695318+INswon@users.noreply.github.com>
2025-03-17 02:15:12 +0900

    feat: user_batch_listを表示できるようにルーティングの追加
    - APIの呼び出し用のテンプレート作成のためのルーティング

--- missions/urls.py
+++ missions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import MissionListView, UserMissionProgressView, BatchListView
+from .views import MissionListView, UserMissionProgressView, BatchListTemplateView, BatchListView
 
 app_name = 'missions'
 
 urlpatterns = [
     path('', MissionListView.as_view(), name='mission_list'),
-    path("user_batches/", BatchListView.as_view(), name="user_batch_list"),
+    path("user_batches/", BatchListView.as_view(), name="user_batches"),
+    path("user_batch_list/", BatchListTemplateView.as_view(), name="user_batch_list"),
     path('progress/', UserMissionProgressView.as_view(), name='user_mission_progress'),
 ]
