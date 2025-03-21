543f8d0864bd095fe0b5493724363859cd8f700b
INswon <152695318+INswon@users.noreply.github.com>
2025-03-21 19:32:44 +0900

    feat: 映画タイトルを踏まえて映画作品の取得ビューのルーティング設定の追加

--- movies/urls.py
+++ movies/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import UserMovieListView,MovieRecordDetailView, MovieRecordCreateView, MovieRecordDeleteView, MovieRecordEditView
+from .views import UserMovieListView, MovieRecordDetailView, MovieRecordCreateView, MovieRecordDeleteView, MovieRecordEditView
+from .services import MovieSerchView
 
 app_name = 'movies'
 
 urlpatterns = [
     path('home/', UserMovieListView.as_view(),name='home'),
+    path('search/', MovieSerchView.as_view(), name='search_movies'),
     path('detail/<int:pk>/',MovieRecordDetailView.as_view(), name='detail'),
     path('create/',MovieRecordCreateView.as_view(), name="create"),
     path('delete/<int:pk>/',MovieRecordDeleteView.as_view(), name="delete"),
