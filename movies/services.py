30c56c5b979079807b04f85c8cd878d431a48bf2
INswon <152695318+INswon@users.noreply.github.com>
2025-03-23 11:33:55 +0900

    refactor: APIで映画データの取得ロジック作成（Viewの責務を整理）

--- movies/services.py
+++ movies/services.py
@@ -1,23 +1,21 @@
-from django.http import JsonResponse
-from django.views import View
 import requests
 from decouple import config
 
-#環境変数からAPIキーの取得()
-TMDB_API_KEY = config('TMDB_API_KEY')  
-TMDB_ACCESS_TOKEN = config('TMDB_ACCESS_TOKEN')
+# 環境変数からAPIキーの取得
+TMDB_API_KEY = config("TMDB_API_KEY")  
 BASE_URL = "https://api.themoviedb.org/3/"
 
-#映画タイトルを検索するクラスベースビュー
-class MovieSerchView(View):
-    def get(self, request, *args, **kwargs):
-        query = request.get("query")
+# TMDb API から映画データ取得するサービス
+class TmdbMovieService:
+
+    @staticmethod
+    def search(query):
         if not query:
-            return JsonResponse({"error":"検索ワードを入力してください"}, status=400)
-        
+            return []
+
         url = f"{BASE_URL}search/movie"
         params = {
-            "api_key": TMDB_API_KEY,  
+            "api_key": TMDB_API_KEY,
             "query": query,
             "language": "ja-JP"
         }
@@ -25,9 +23,9 @@ class MovieSerchView(View):
         try:
             response = requests.get(url, params=params, timeout=5)
             response.raise_for_status()
-            movies = response.json().get("results", [])
-
-            return JsonResponse({"movies": movies}, status=200)
-        
+            data = response.json()
+            print(f"APIレスポンス: {data}") 
+            return data.get("results", [])
         except requests.exceptions.RequestException as e:
-            return JsonResponse({"error": f"APIエラー: {e}"}, status=500)
+            print(f"APIエラー: {e}")
+            return []
