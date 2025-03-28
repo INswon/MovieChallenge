b1d4266caf1bcc76bb282179950f12faf5a1ba10
INswon <152695318+INswon@users.noreply.github.com>
2025-03-28 21:50:45 +0900

    feat: 3ジャンル制覇ミッションのシグナル処理を追加　refs #3

--- missions/signals.py
+++ missions/signals.py
@@ -1,26 +1,30 @@
-import logging
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from movies.models import UserMovieRecord
 from missions.services import MissionService
 from django.utils.timezone import localtime
 
-logger = logging.getLogger(__name__)
-
 @receiver(post_save, sender=UserMovieRecord)
 def auto_assign_batch_on_movie_save(sender, instance, created, **kwargs):
-    if created:
-        user = instance.user
-        logger.debug(f"シグナル実行: {instance.title} (User: {user.username})")
+    if not created:
+        return
 
-        #「3本の映画視聴達成」のミッション
-        MissionService.grant_batch_for_mission(user, "3本の映画達成")
-        logger.debug(f"MissionService.grant_batch_for_mission() 実行完了: {user.username}")
+    user = instance.user
 
-        #「1日3本の映画視聴達成」のミッション
-        today = instance.date_watched  
-        today_movie_count = UserMovieRecord.objects.filter(user=user, date_watched=today).count()
+    #「3本の映画視聴達成」のミッション
+    MissionService.grant_batch_for_mission(user, "3本の映画達成")
 
-        if today_movie_count >= 3:
-            MissionService.grant_batch_for_mission(user, "1日3本の映画視聴達成")
-            logger.debug(f"1日3本の映画視聴達成バッチを付与: {user.username} ({today_movie_count}本)")
+    #「1日3本の映画視聴達成」のミッション
+    today = instance.date_watched  
+    today_movie_count = UserMovieRecord.objects.filter(user=user, date_watched=today).count()
+
+    if today_movie_count >= 3:
+        MissionService.grant_batch_for_mission(user, "1日3本の映画視聴達成")
+        print(f"1日3本の映画達成バッジを付与: {user.username} ({today_movie_count}本)")
+
+    #「３ジャンルの映画鑑賞達成」のミッション
+    success, message = MissionService.check_and_assign_genre_batch(user)
+    if success:
+        print(f"3ジャンル制覇バッジを付与 ユーザー: {user.username} - {message}")
+    else:
+        print(f"3ジャンル未達成ユーザー: {user.username} - {message}")
