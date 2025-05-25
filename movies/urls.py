9c544dde8ccfeeddae69cc7c73a0fbb5a18eeb8a
INswon <152695318+INswon@users.noreply.github.com>
2025-05-26 00:01:04 +0900

    feat:感情クエリからアーカイブページへ自動リダイレクトするルーティングを追加 #32
    - redirect_to_mood_archive 関数を定義し、クエリ文字列 (?mood=感情名) に応じて該当の感情アーカイブページへリダイレクト
    - ルーティング 'mood_search/' にて redirect_to_mood_archive を紐づけ
    - フォームやURLから直接感情名が入力された場合の遷移できるように対応

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,7 @@
+from urllib.parse import quote
 from django.urls import path
 from .views import UserMovieListView, MovieSearchView, MovieRecordDetailView, MoodArchiveView, ReviewPageView, ThanksPageView, ReviewLikeView, MovieRecordCreateView, MovieRecordDeleteView, MovieRecordEditView
+from .views import redirect_to_mood_archive 
 from . import views
 
 app_name = 'movies'
@@ -7,7 +9,7 @@ app_name = 'movies'
 urlpatterns = [
     path('home/', UserMovieListView.as_view(),name='home'),
     path('search/', MovieSearchView.as_view(), name='movie_search'),
-    path('mood_search/', UserMovieListView.as_view(), name='mood_search'),
+    path('mood_search/', redirect_to_mood_archive, name='mood_search'),
     path('mood_archive/<str:mood_name>/',MoodArchiveView.as_view(), name='mood_archive'),
     path('movie_create/', views.create_movie_record, name='movie_create'),
     path('detail/<int:pk>/',MovieRecordDetailView.as_view(), name='detail'),
