caa2cc7fc3d01f048a0c45c9846968201f29436e
Kenta Nakaya <152695318+INswon@users.noreply.github.com>
2025-06-02 18:28:50 +0900

    refactor: Home画面(映画記録一覧画面)はmoves_appに作成のため削除

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('signup/', SignupView.as_view(), name='signup'),
-    path('home/', UserMovieListview.as_view(),name='home'),
     path('goals/create/',ProgressGoalCreateView.as_view(), name='progress_goal_create'),
     path('goals/update/<int:pk>/', ProgressGoalUpdateView.as_view(), name='progress_goal_update'),
 ]
