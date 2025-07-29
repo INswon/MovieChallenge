e2f1071e2b9cb3cc6b97ced4de596a2d60c9663a
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-07-29 23:33:10 +0900

    test: (異常系)存在しない感情IDを指定した場合404が返却される検証#34

---
+++ movies/tests/unit/test_mood_archive.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from movies.models import Mood
+from django.urls import reverse
+
+# 感情アーカイブページの遷移に関するテストケース
+class MoodPageTestCase(TestCase):
+    def setUp(self):
+        # ログインユーザー作成
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+        self.client.login(username="testuser", password="testpass")
+
+        # テスト用の感情データ作成
+        self.mood = Mood.objects.create(name="癒された")
+
+    # (正常系) 感情名「癒された」でアクセスした場合、200が返る
+    def test_mood_page_returns_200(self):
+        url = reverse("movies:mood_archive", kwargs={"mood_name": self.mood.name})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    # (異常系) 存在しない感情名でアクセスした場合、404が返る
+    def test_mood_page_returns_404_for_invalid_id(self):
+        url = reverse("movies:mood_archive", kwargs={"mood_name": "存在しない感情"})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
