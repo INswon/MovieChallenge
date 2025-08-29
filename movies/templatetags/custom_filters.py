31886976f2d8318bda3274a6f80a6545ef4712d2
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-08-29 13:36:20 +0900

    feat: get_item フィルタを追加 (movies/templatetags/custom_filters.py)#34
    - dict からキーを取得するカスタムフィルタを実装
    
    - キーが存在しない場合は空文字を返す
    
    - home.html 、mood_archive.hmtlの感情ボタン描画で利用

--- movies/templatetags/custom_filters.py
+++ movies/templatetags/custom_filters.py
@@ -4,9 +4,6 @@ register = template.Library()
 
 @register.filter(name="get_item")
 def get_item(mapping, key):
-    """
-    dict からキーで取り出す。見つからなければ空文字。
-    """
     try:
         return mapping.get(key, "")
     except AttributeError:
