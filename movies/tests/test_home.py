809f6fe76680d948a2725b9b6b2466f5506a42e8
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-09-11 20:10:04 +0900

    test:未ログインユーザーがホーム画面にアクセス時、LOGIN_URL('/users/login/')にリダイレクトされることの検証 #42

--- movies/tests/test_home.py
+++ movies/tests/test_home.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.shortcuts import resolve_url
+from django.conf import settings
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
@@ -12,6 +14,15 @@ class HomeViewTests(TestCase):
     # ユーザーがログインした際にホーム画面にアクセスできることの確認
     def test_access_when_logged_in(self):
         self.client.login(username="u", password="p")
-        res = self.client.get(reverse("movies:home"))
-        self.assertEqual(res.status_code,200)
-        self.assertTemplateUsed(res,"movies/home.html")
+        response = self.client.get(reverse("movies:home"))
+
+        self.assertEqual(response.status_code,200)
+        self.assertTemplateUsed(response, "movies/home.html")
+    
+    # 未ログインユーザーがホーム画面にアクセスした時LOGIN_URLにリダイレクトされることの確認
+    def test_redirects_when_not_logged_in(self):
+        path = reverse("movies:home")
+        response =  self.client.get(path)
+
+        expected_url = f"{resolve_url(settings.LOGIN_URL)}?next={path}"
+        self.assertRedirects(response, expected_url)
