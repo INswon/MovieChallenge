a85315cabc0dfe2adf5eeed2a9a0fd470ae09de1
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-09-12 11:23:04 +0900

    test:「気分を選んでおすすめを見る」ボタンを押して、映画推薦画面に画面遷移することの検証 #42

--- movies/tests/test_home.py
+++ movies/tests/test_home.py
@@ -26,3 +26,17 @@ class HomeViewTests(TestCase):
 
         expected_url = f"{resolve_url(settings.LOGIN_URL)}?next={path}"
         self.assertRedirects(response, expected_url)
+
+    # 「気分を選んでおすすめを見る」ボタンを押して、映画推薦画面に画面遷移することの検証
+    def test_main_cta_navigates_to_recommend_page(self):
+        self.client.login(username="u", password="p")
+        path = reverse("movies:home")
+        recommend_path = reverse("movies:recommend", kwargs={"recommend_name": "healed"})
+
+        response = self.client.get(path)
+        self.assertContains(response, 'data-testid="recommend"')
+        self.assertContains(response, f'href="{recommend_path}"')
+        resp2 = self.client.get(recommend_path)
+
+        self.assertEqual(resp2.status_code, 200)
+        self.assertTemplateUsed(resp2, "movies/recommend.html")
