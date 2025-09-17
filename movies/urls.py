833c974368ce9d5d565e2a1b6e2009da7cd82954
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-09-17 13:16:54 +0900

    refactor: URLパラメータ名を recommend_name から category に変更 #44 - 映画推薦機能でカテゴリごとにおすすめを出せるように命名を整理

--- movies/urls.py
+++ movies/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
 
     # 5. 映画推薦
     path('recommend/',RecommendSelectView.as_view(), name='recommend_select'),
-    path('recommend/<str:recommend_name>/',RecommendListView.as_view(), name='recommend'),
+    path('recommend/<str:category>/',RecommendListView.as_view(), name='recommend'),
     
     # 6. レビュー
     path('review/<int:pk>/', ReviewPageView.as_view(), name='review'),
