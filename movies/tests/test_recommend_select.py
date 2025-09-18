0c76237160cfe0d98fca0ff39edeba7bea6706ad
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-09-18 17:09:35 +0900

    test:感情選択画面のアクセスとテンプレート表示の検証 #46

--- movies/tests/test_recommend_select.py
+++ movies/tests/test_recommend_select.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse,resolve
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 class Recommend_SelectTests(TestCase):
+    #　作成データ
+    def setUp(self):
+        self.user = User.objects.create_user(username="u", password="p")
     
     # 気分選択画面のURL逆引き、ルーティングが適切である確認
     def test_reverse_and_resolve(self):
@@ -10,3 +15,10 @@ class Recommend_SelectTests(TestCase):
 
         resolver_match= resolve("/movies/recommend/")
         self.assertEqual(resolver_match.view_name, "movies:recommend_select")
+
+    # ログイン済みユーザーが感情選択画面にアクセスでき、期待テンプレートで描画されることを確認
+    def test_status_and_template(self):
+        self.client.login(username = "u", password = "p")
+        response = self.client.get(reverse("movies:recommend_select"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "movies/recommend_select.html")
