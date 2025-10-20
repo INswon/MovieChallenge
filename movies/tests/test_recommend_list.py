d78ba72976b7a7677dbd2d7694bbc5adb489353c
Kenta Nakaya <kenta.nakaya.dev@gmail.com>
2025-10-20 20:24:54 +0900

    fix: テストコード内のルーティング名を recommend_list → recommend に変更 #52 - urls.py の定義に合わせて修正

--- movies/tests/test_recommend_list.py
+++ movies/tests/test_recommend_list.py
@@ -14,17 +14,17 @@ class Recommend_ListTests(TestCase):
     # 気分別おすすめ映画一覧のURL逆引き、ルーティングが適切である確認　
     def test_reverse_and_resolve(self):
         for key in RECOMMEND_CATEGORY.keys():
-            url = reverse("movies:recommend_list", args=[key])
+            url = reverse("movies:recommend", args=[key])
             match = resolve(url)
 
-            self.assertEqual(match.view_name, "movies:recommend_list")
+            self.assertEqual(match.view_name, "movies:recommend")
             self.assertEqual(match.kwargs, {"category":key})
 
     # ログイン済みユーザーが気分別おすすめ映画一覧にアクセスでき、期待テンプレートで描画されることを確認　
     def test_status_and_template(self):
         self.client.login(username = "u", password = "p")
         for key in RECOMMEND_CATEGORY.keys():
-            url = reverse("movies:recommend_list", args=[key])
+            url = reverse("movies:recommend", args=[key])
             response = self.client.get(url)
 
             self.assertEqual(response.status_code, 200)
@@ -33,7 +33,7 @@ class Recommend_ListTests(TestCase):
     # 未ログインユーザーはリダイレクトされることの確認　
     def test_redirects_when_not_logged_in(self):
         for key in RECOMMEND_CATEGORY.keys():
-            path = reverse("movies:recommend_list", args=[key])
+            path = reverse("movies:recommend", args=[key])
             response = self.client.get(path)
 
             expected_url = f"{resolve_url(settings.LOGIN_URL)}?next={path}"
@@ -43,7 +43,7 @@ class Recommend_ListTests(TestCase):
     def test_heading_shows_category_label(self):
         self.client.login(username="u", password="p")
         for key, category_data in RECOMMEND_CATEGORY.items():
-            url = reverse("movies:recommend_list", args=[key])
+            url = reverse("movies:recommend", args=[key])
             response = self.client.get(url)
             expected_heading = f"「{category_data['label']}」おすすめの作品はこちら"
             self.assertContains(response, expected_heading)
@@ -52,7 +52,7 @@ class Recommend_ListTests(TestCase):
     def test_all_movie_titles_render(self):
         self.client.login(username="u", password="p")
         for key in RECOMMEND_CATEGORY.keys():
-            url = reverse("movies:recommend_list", args=[key])
+            url = reverse("movies:recommend", args=[key])
             response = self.client.get(url)
             for mv in RECOMMEND_MOVIE[key]:
                 self.assertContains(response, mv["title"])
